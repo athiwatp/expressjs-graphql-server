ca480e55a569230324a03c100c3069da9f4362ad
Igor Ribeiro <U06IXR@macys.com>
2016-01-10 10:16:43 -0200

    Enable CORS

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 var express = require('express')
 var Schema = require('./schema')
 var graphQLHTTP = require('express-graphql')
+var cors = require('cors')
 
 var app = express()
+app.use(cors())
 app.use('/', graphQLHTTP({
   schema: Schema,
   pretty: true,
   graphiql: true
 }))
+
 app.listen(process.env.PORT || 8080, (err) => {
   if (err) {
     console.error(err)
